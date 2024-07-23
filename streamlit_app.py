ea45ad8fee75364ecc07a95d55d5b0b008212c1a
fahrahaqilaha <136235076+fahrahaqilaha@users.noreply.github.com>
2024-07-23 16:04:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,8 @@
-import streamlit as st
-from openai import OpenAI
-
-# Show title and description.
-st.title("💬 Chatbot")
+I
+#  description.
+st.title("Standar Baku Mutu Hygine Sanitasi Air Minum")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "Aplikasi ini untuk mengukur standar baku mutu sample untuk Hygine Air Minum Baku Mutu KEMENKES NO 32 2017."
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
